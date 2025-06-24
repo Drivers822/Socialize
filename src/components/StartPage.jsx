da4787d6dfd50a029d294646b8da6c0ccaab7c0a
rushikeshkekane123 <rushikeshkekane5@gmail.com>
2025-06-24 19:10:26 +0530

    responsive

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -5,19 +5,18 @@ import '../Styles/StartPage.css'; // Import CSS
 const StartPage = () => (
   <div className="start-page">
     <div className="overlay">
-      <div className="container">
-        <h2 className="title">Select Your Option</h2>
-        <div className="button-group">
-          <Link to="/login" className="btn-link">
-            <button className="action-btn">Login</button>
-          </Link>
-          <Link to="/register" className="btn-link">
-            <button className="action-btn">Register</button>
-          </Link>
-        </div>
+      <h2 className="title">Select Your Option</h2>
+      <div className="button-group">
+        <Link to="/login" className="btn-link">
+          <button className="action-btn">Login</button>
+        </Link>
+        <Link to="/register" className="btn-link">
+          <button className="action-btn">Register</button>
+        </Link>
       </div>
     </div>
   </div>
+
 );
 
 export default StartPage;
